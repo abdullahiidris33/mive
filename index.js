9687ad443e48e4acf506d3ebdcb26c9e77f44ed5
abdullahiidris33 <134279435+abdullahiidris33@users.noreply.github.com>
2023-06-15 05:50:26 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const soja = [{
+    name: 'abdullahi idris',
+    country: 'nigeria',
+
+},
+{
+    name: 'nouman tasif',
+    country: 'pakistan',
+    
+},
+{
+    name: 'yomif kumsisa',
+    country: 'ethopian',
+
+
+}, 
+{
+    name: 'ratdzio',
+    country: 'south Africa',
+}
+];
+
+console.log(soja)
